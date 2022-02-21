c99df4ba0852fa0d383847ec82a2344d8fd72137
joseungjin <jn5947@naver.com>
2022-02-21 22:38:49 +0900

    git add

--- index.js
+++ index.js
@@ -2,15 +2,35 @@ const express =require('express')
 const res = require('express/lib/response')
 const app =express()
 const port =5000
+const bodyParser=require('body-parser')
 
+const config = require('./config/key');
+const {User} =require("./models/User")
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(bodyParser.json());
 
 const mongoose =require('mongoose')
-mongoose.connect('mongodb+srv://joseungjin:wh5947@boilerplate.r7lah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+
+mongoose.connect(config.mongoURI,{
 }).then(()=>console.log('MongoDB connect.....'))
 .catch(err => console.log(err))
 
-app.get('/',(req,res) => res.send('Hello Word! 안녕하세요'))
+app.get('/',(req,res) => res.send('Hello Word! 안녕하세요 하하핳하ㅏ'))
 
+app.post('/register',(req,res)=>{
+
+    const user = new User(req.body)
+
+    user.save((err,useInfo)=>{
+        if(err) return res.json({success:false,err})
+        return res.status(200).json({
+            success:true
+
+        })
+    })
+})
 app.listen(port,() => console.log('Example app listening on port',port,'!') )
 
 
